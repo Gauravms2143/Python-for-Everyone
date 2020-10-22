98d17fc6bad57701d248415be46966bffc952442
GauravMS11 <72162639+GauravMS11@users.noreply.github.com>
2020-10-22 10:14:29 +0530

    Docs for DNS
    its always best option to add docs string in code, it will help you for the long term of project.

--- dns1.py
+++ dns1.py
@@ -1,9 +1,10 @@
 import socket
-
+"""Take input as host name from user"""
 ip=input("enter host name:")
 addr1=socket.gethostbyname(ip)
 print(addr1)
 add=input("enter ip address:")
+"""Gives IP address of host by its name."""
 addr2=socket.gethostbyaddr(add)
 print(addr2)
 
